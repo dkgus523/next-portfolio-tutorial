e4d508575e95758fee66f31718cd3a8bcb8bf037
dkgus523 <73331053+dkgus523@users.noreply.github.com>
2024-11-12 15:49:37 +0900

    fix: all content edit

--- components/dark-mode-toggle-button.js
+++ components/dark-mode-toggle-button.js
@@ -1,43 +1,50 @@
-import { useTheme } from 'next-themes'
+import { useTheme } from "next-themes";
 
-export default function DarkModeToggleButton(){
+export default function DarkModeToggleButton() {
+  const { theme, setTheme } = useTheme();
 
-    // theme : 현재 값 가져오기 getter
-    // setTheme : 현재 값 바꾸기 setter
-    const { theme, setTheme } = useTheme()
-
-    return (
-        <>
-            <button
-                className="
-                    inline-flex items-center
-               border-0 py-1 px-3 rounded text-base mt-4 md:mt-0
-                focus:outline-none
-                bg-gray-100
-                hover:bg-gray-50
-                hover:text-orange-500
-                dark:bg-slate-600
-                dark:text-slate-400
-                dark:hover:bg-slate-700
-                dark:hover:text-yellow-300
-                "
-                type="button"
-                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-            >
-                {/* 라이트 모드 */}
-                <svg xmlns="http://www.w3.org/2000/svg"
-                        className="visible dark:invisible dark:h-0 dark:w-0 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clipRule="evenodd" />
-                </svg>
-
-                {/* 다크모드 */}
-                <svg xmlns="http://www.w3.org/2000/svg"
-                     className="visible dark:visible dark:h-5 dark:w-5 h-0 w-0"
-                     viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-                </svg>
-
-            </button>
-        </>
-    );
+  // theme : 현재 값 가져오기 getter
+  // setTheme : 현재 값 바꾸기 setter
+  return (
+    <>
+      <button
+        className="inline-flex items-center 
+            bg-gray-100 border-0 py-1 px-3 rounded text-base mt-4 md:mt-0
+            focus:outline-none 
+            bg-gray-100
+            hover:bg-gray-50
+            hover:text-orange-500
+            dark:bg-slate-600
+            dark: text-slate-400
+            dark:hover:bg-slate-700
+            dark:hover:text-yellow-300
+            "
+        type="button"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      >
+        {/* 라이트모드 */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="visible dark:invisible dark:h-0 dark:w-0 h-5 w-5"
+        >
+          <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+        </svg>
+        {/* 다트모드 */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="invisible dark:visible dark:h-5 dark:w-5 h-0 w-0"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+    </>
+  );
 }
--- components/home/hero.js
+++ components/home/hero.js
@@ -1,29 +1,37 @@
-import Animation from "./animation";
-import Link from 'next/link';
+import dynamic from "next/dynamic";
+import Link from "next/link";
+
+// Animation을 동적 로딩하도록 설정
+const Animation = dynamic(() => import("./animation"), { ssr: false });
 
 export default function Hero() {
-    return (
-        <>
-            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                안녕하세요 빡빡이입니다!
-                <br className="hidden lg:inline-block"/>
-                오늘도 빡코딩?
-            </h1>
-            <p className="mb-8 leading-relaxed">
-            인간의 그들의 가장 피다. 천고에 것은 끝까지 풍부하게 그것을 풀이 길지 피다. 굳세게 인생의 사람은 따뜻한 시들어 끓는 그들에게 튼튼하며, 위하여, 듣는다. 가치를 그림자는 이것은 자신과 듣기만 소리다.이것은 이것이다. 얼마나 위하여 뜨거운지라, 철환하였는가? 위하여서, 그들의 피고 되려니와, 봄바람을 남는 영락과 천지는 같은 것이다. 만물은 황금시대를 투명하되 그림자는 전인 우리는 가는 무엇을 있으랴? 이상 이것은 인간의 것은 청춘의 황금시대다. 사랑의 풀이 대한 위하여서 것이다.
-            </p>
-            <div className="flex justify-center">
-                <Link href="/projects">
-                    <a className="btn-project">
-                        프로젝트 보러가기
-                    </a>
-                </Link>
-            </div>
-            </div>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                <Animation/>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+          안녕하세요.
+          <br className="hidden lg:inline-block" />
+          프론트엔드 개발자로 성장하고 싶은 한아현입니다.
+        </h1>
+        <p className="mb-8 leading-relaxed">
+          저는 Node.js 기반의 백엔드 경험을 바탕으로, React와 같은 최신
+          프론트엔드 프레임워크와 반응형 웹 디자인에 관심을 두고 UI/UX 중심의
+          프론트엔드 개발자로 성장하고자 합니다. 이전 직장에서 Playauto 2.0
+          솔루션 개발과 유지보수, 오픈API 설계 등 프로젝트를 담당하며 사용자
+          관리와 보안 강화에 기여한 바 있습니다. 웹 퍼포먼스 최적화와 UX 개선에
+          열정을 가지고 있으며, 협업과 소통을 중요하게 여깁니다. 작은 디테일까지
+          놓치지 않는 꼼꼼함과 책임감을 통해 프로젝트에 기여하는 개발자가
+          되겠습니다.
+        </p>
+        <div className="flex justify-center">
+          <Link href="/projects" legacyBehavior>
+            <a className="btn-project">프로젝트 보러가기</a>
+          </Link>
+        </div>
+      </div>
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <Animation />
+      </div>
+    </>
+  );
 }
