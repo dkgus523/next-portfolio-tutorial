df053cd0a561bcbb23ad1bde18b423afe547a355
dkgus523 <73331053+dkgus523@users.noreply.github.com>
2024-11-14 14:37:29 +0900

    fix: 홈 화면 타이핑 효과 추가

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 import Link from "next/link";
+import Typing from "./typing";
 
 // Animation을 동적 로딩하도록 설정
 const Animation = dynamic(() => import("./animation"), { ssr: false });
@@ -8,11 +9,7 @@ export default function Hero() {
   return (
     <>
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-          안녕하세요.
-          <br className="hidden lg:inline-block" />
-          프론트엔드 개발자로 성장하고 싶은 한아현입니다.
-        </h1>
+        <Typing />
         <p className="mb-8 leading-relaxed">
           저는 Node.js 기반의 백엔드 경험을 바탕으로, React와 같은 최신
           프론트엔드 프레임워크와 반응형 웹 디자인에 관심을 두고 UI/UX 중심의
@@ -35,3 +32,5 @@ export default function Hero() {
     </>
   );
 }
+
+// p 안 내용을 about me 에 넣고 여기는 타이핑되는 애니메이션 넣기
---
+++ components/home/typing.js
@@ -0,0 +1,48 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState, useEffect } from "react";
+
+export default function DynamicTyping() {
+  const [displayText, setDisplayText] = useState(
+    ""
+  ); // 보여질 텍스트
+  const [isDeleting, setIsDeleting] = useState(false); // 삭제 중인지
+  const [loopNum, setLoopNum] = useState(0);
+  const [typingSpeed, setTypingSpeed] = useState(50);
+
+  const dynamicTexts = [
+    "안녕하세요","프론트엔드 개발자로 성장하고 싶은 한아현입니다.",
+  ];
+  const currentText = dynamicTexts[loopNum % dynamicTexts.length];
+
+  useEffect(() => {
+    const handleTyping = () => {
+      if (!isDeleting && displayText.length < currentText.length) {
+        // 타이핑 중
+        setDisplayText(currentText.substring(0, displayText.length + 1));
+      } else if (isDeleting && displayText.length > 0) {
+        // 삭제 중
+        setDisplayText(currentText.substring(0, displayText.length - 1));
+      } else if (!isDeleting && displayText.length === currentText.length) {
+        // 타이핑 완료 후 대기
+        setTimeout(() => setIsDeleting(true), 1500);
+      } else if (isDeleting && displayText.length === 0) {
+        // 삭제 완료 후 다음 텍스트로
+        setIsDeleting(false);
+        setLoopNum(loopNum + 1);
+      }
+    };
+
+    const timer = setTimeout(handleTyping, typingSpeed);
+
+    return () => clearTimeout(timer);
+  }, [currentText, displayText, isDeleting, loopNum, typingSpeed]);
+
+  return (
+    <div className="heading">
+      <div>
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">{displayText}</h1>
+        <span className="cursor">|</span>
+      </div>
+    </div>
+  );
+}
